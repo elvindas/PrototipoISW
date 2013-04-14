c9b1667fbdf1331fc42bcd10bcd82d5104ca7183
Elnier <elvindasmora@gmail.com>
2013-04-14 16:08:28 -0600

    Ajustes

--- appPrototipoISW/appPrototipoISW/Vistas/FrmPrincipal.cs
+++ appPrototipoISW/appPrototipoISW/Vistas/FrmPrincipal.cs
@@ -82,7 +82,7 @@ namespace appPrototipoISW.Vistas
             //Aqui es cuando hacemos uso de la variable renglon, la inicializamos diciendole que va a ser un nuevo renglon de la Tabla que es de tipo DataTable 
             Renglon2 = Tabla2.NewRow();
             //Aqui simplemente le agregamos el renglon nuevo con los valores que nosotros querramos, para hacer referencia a cada columna podemos utilizar los indices de cada columna 
-            Renglon2[0] = "001";
+            Renglon2[0] = "01";
             Renglon2[1] = "Lorenso";
             Renglon2[2] = "Pardo Suizo";
             Renglon2[3] = "Macho";
